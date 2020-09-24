d249eccd6fcfe48a36dbcad088620dd161c0fe81
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2020-09-24 15:09:17 +0200

    starting developing perturb subroutine

--- Progetto/scripts/blackBox.py
+++ Progetto/scripts/blackBox.py
@@ -1,6 +1,6 @@
 
 import tensorflow as tf
-from scripts.preprocessing import Preprocesser as preprocesser
+from scripts.preprocessing import Preprocesser
 import pickle
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -8,24 +8,22 @@ class BlackBox:
     
     def __init__(self):
         with open('pickle\\tokenizer.pickle', 'rb') as f:
-            self.__tokenizer, self.__maxlen = pickle.load(f)
-        
+            tokenizer, maxlen = pickle.load(f)
+            self.__tokenizer = tokenizer
+            self.__maxlen = maxlen
         self.__model = tf.keras.models.load_model("models\\best_model.h5")
         
     def __text_preprocessing(self, text):
-        text = preprocesser.text_preprocessing(text)      
+        return Preprocesser.text_preprocessing(text)      
         
     def __tokenize(self, text):
-        
-        sequences = self.__tokenize.texts_to_sequences(text)
-
+        sequences = self.__tokenizer.texts_to_sequences(text)
         return pad_sequences(sequences, maxlen = self.__maxlen)
         
     def predict_sentiment(self, text):
         text = self.__text_preprocessing(text)
-        text = [text]
-        text = self.__tokenize(text)
-        return self.__model.predict(text).take(0)
+        seq = self.__tokenize([text])
+        return self.__model.predict(seq).take(0)
     
-    def evaluate(self, test, label):
-        self.__model.evaluate(test,label)
+#    def evaluate(self, test, label):
+#        self.__model.evaluate(test,label)

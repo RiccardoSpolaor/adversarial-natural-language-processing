3e782241dfe473d8ee74954f15a066a1c144ff51
Riccardo Spolaor <riccardo.spolaor94@gmail.com>
2021-01-11 13:59:04 +0100

    added notebook with test data elaboration

--- Progetto/licensed_scripts/lm_1b_eval.py
+++ Progetto/licensed_scripts/lm_1b_eval.py
@@ -1,4 +1,4 @@
-# Author: Riccardo Spolaor (864877@stud.unive.it)
+# Author: Moustafa Alzantot (malzantot@ucla.edu)
 # Copyright 2016 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
